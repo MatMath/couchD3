9d2972ee6347a0ccbe719e402b921c9f94b00917
MatMath <mathieu.k.legault@gmail.com>
2014-09-07 10:45:56 -0400

    Initial Angular folder configuration with Yeoman.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name couchD3App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the couchD3App
+ */
+angular.module('couchD3App')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
