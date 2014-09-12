ad8d23bb255c24bf8edd84b80d2c70781a530aaa
MatMath <mathieu.k.legault@gmail.com>
2014-09-12 11:12:37 -0400

    Modification in lineplusbarwithfocuschart to handle array of object instead of array of array. This will make the map process more user friendly in the future when we map from a mapreduce (couchDB).

--- app/scripts/directives/couch-json.js
+++ app/scripts/directives/couch-json.js
@@ -12,7 +12,10 @@ angular.module('couchD3App').directive('couchJson', function() {
 	var controller = function($scope) {
 		console.log('Hehe Im in the controller.', $scope);
 		// $scope.fetchingTo = 'https://corpusdev.lingsync.org/mathieu-firstcorpus';
-		$scope.fetchingTo = 'https://localhost:6984/mathieu-firstcorpus/_design/pages/_view/byStage';
+		$scope.fetchingInfo = {};
+		$scope.fetchingInfo.dbPath = 'https://localhost:6984/stage02-fake-data/';
+		$scope.fetchingInfo.mapReduce = '_design/extract2/_view/datagraph?group=true';
+		$scope.fetchingTo = $scope.fetchingInfo.dbPath + $scope.fetchingInfo.mapReduce;
 
 
 		$scope.httpGet = function(theUrl) {
